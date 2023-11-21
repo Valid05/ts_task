c95152b85fafee19e719ba52b321b270a4a30107
Valid05 <bruh4268valid@gmail.com>
2023-11-21 16:26:18 +0300

    v

---
+++ ts-set-types/1.ts
@@ -0,0 +1,12 @@
+type task = {
+    bootcampName: string;
+    bootcampAge: number;
+    isDarkTheme: boolean;
+    numberOfCourses : number;
+    courseNames: object;
+    studentGrades: object;
+    price: number;
+    negativeNumber: number;
+    studentPhone: string;
+    studentAddress:null
+}
---
+++ ts-set-types/2.ts
@@ -0,0 +1,21 @@
+// укажи правильные типы для параметров функции и тип возвращемых данных
+
+function sum(a:number, b:number):number {
+  return a + b;
+}
+
+function toUpperCase(text:string):string {
+  return text.trim().toUpperCase();
+}
+
+function firstLetter(name: string[]) {
+  return name[0];
+}
+
+function getFullName(firstName:string, lastName:string):string {
+  return firstName + ' ' + lastName;
+}
+
+function getFirstElement(arr:string[]) {
+  return arr[0];
+}
